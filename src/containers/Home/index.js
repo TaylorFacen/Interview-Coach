225cc412e15b8e7a91e66ffd1267e021fe382a33
TaylorFacen <44216029+TaylorFacen@users.noreply.github.com>
2020-07-19 01:37:57 -0400

    Made hero image fluid

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -24,6 +24,7 @@ export default () => (
                 <Image 
                     src = { heroImage } 
                     alt = "Hero image" 
+                    fluid
                 />
             </Col> 
         </Row>
