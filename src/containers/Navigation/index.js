317e786cd9be1389edc852dc4d28636f0fe454af
TaylorFacen <44216029+TaylorFacen@users.noreply.github.com>
2020-07-26 12:24:27 -0400

    Added link to support project

--- src/containers/Navigation/index.js
+++ src/containers/Navigation/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
+import logo from '../../images/interview-coach-logo.png'
 import './Navigation.css'
 
 export default () => {
     return (
         <Navbar bg = "white" className = "Navigation">
-            <Navbar.Brand href="/">Interview Coach</Navbar.Brand>
+            <Navbar.Brand href="/">
+                <img 
+                    src = { logo }
+                    alt = "InterviewCoach logo"
+                    width = "50"
+                    height = "50"
+                />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link target="_blank" href = "https://www.buymeacoffee.com/interviewcoach">Support InterviewCoach</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
